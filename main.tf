5585f428ac37fe908655c4e8896228cd345e1764
Najib Aseri <najibaseri@gmail.com>
2023-06-02 19:21:10 +0800

    bucket

--- main.tf
+++ main.tf
@@ -21,11 +21,19 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+# resource "aws_instance" "ubuntu" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = var.instance_name
+#   }
+# }
+resource "aws_s3_bucket" "example" {
+  bucket = "bucketoh"
 
   tags = {
-    Name = var.instance_name
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
