06693247d103c45c393d46a4fa0f9a0295e06cee
Najib Aseri <najibaseri@gmail.com>
2023-06-02 19:48:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,6 +90,5 @@ resource "aws_db_instance" "default" {
   instance_class       = "db.t3.micro"
   username             = "foo"
   password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
