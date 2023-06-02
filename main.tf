4aab57b125aa420ad4b94f3f69cd11bf7c49b1b8
Najib Aseri <najibaseri@gmail.com>
2023-06-02 19:41:53 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,3 +81,16 @@ resource "aws_dynamodb_table" "my-dynamo-tableohhhhhhhh" {
     Environment = "production"
   }
 }
+
+
+resource "aws_db_instance" "sqldb-instance" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
